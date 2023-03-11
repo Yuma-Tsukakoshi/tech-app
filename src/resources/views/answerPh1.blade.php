b14329b79bb19e9688febee5c30d86e7a25f943e
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-03-11 23:29:03 +0900

    add: layouts 各phごとのヒント一覧レイアウト作成

--- src/resources/views/answerPh1.blade.php
+++ src/resources/views/answerPh1.blade.php
@@ -1,116 +1,90 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TechApp</title>
-  <script src="{{asset('/js/jquery-3.6.1.min.js')}}" defer></script>
-  <script src="{{asset('/js/accordion.js')}}" defer></script>
-</head>
-<body>
-  @include('components.header')
+@extends('layouts.each_ph_answer')
 
-  {{-- このファイルではph1向けに書いているが、本来は初めのボタン選択で中身を各ph仕様に変えていく --}}
-  <section>
-    <div>
-      <h2>week絞り込み</h2>
-      <label for="week">week</label>
-      <input type="text" name="week" id="week">
-      <input type="button" value="検索" id="filter-button">
-    </div>
+@section('answer-items')
+<li class="answer-item" week="1">
+  <div>
+    <h3>week1</h3>
+    <button class="detail-button">詳細</button>
+  </div>
+  <ul class="answer-content-list">
+    <li>
+      <p>Google Chromeをインストールしましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>Visual Studio Codeをインストールしましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>Visual Studio Codeの初期設定(editorの設定と日本語化)は出来ましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
 
-    <ul class="answer-list">
+<li class="answer-item" week="2">
+  <div>
+    <h3>week2</h3>
+    <button class="detail-button">詳細</button>
+  </div> 
+  <ul class="answer-content-list">
+    <li>
+      <p>Google Chromeをインストールしましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>Visual Studio Codeをインストールしましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>Visual Studio Codeの初期設定(editorの設定と日本語化)は出来ましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
 
-      <li class="answer-item" week="1">
-          <div>
-            <h3>week1</h3>
-            <button class="detail-button">詳細</button>
-          </div>
-          <ul class="answer-content-list">
-            <li>
-              <p>Google Chromeをインストールしましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>Visual Studio Codeをインストールしましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>Visual Studio Codeの初期設定(editorの設定と日本語化)は出来ましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-          </ul>
-      </li>
+<li class="answer-item" week="3">
+  <div>
+    <h3>week3</h3>
+    <button class="detail-button">詳細</button>
+  </div> 
+  <ul class="answer-content-list">
+    <li>
+      <p>Gitコマンドをインストールしましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>"GitHubのposse-apにあるリポジトリ「template-ph1-website」を  forkできましたか？
+        ※リンク: https://github.com/posse-ap/ template-ph1-website"</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>forkした「template-ph1-website」をcloneできましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
 
-      <li class="answer-item" week="2">
-          <div>
-            <h3>week2</h3>
-            <button class="detail-button">詳細</button>
-          </div> 
-          <ul class="answer-content-list">
-            <li>
-              <p>Google Chromeをインストールしましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>Visual Studio Codeをインストールしましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>Visual Studio Codeの初期設定(editorの設定と日本語化)は出来ましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-          </ul>
-      </li>
-
-      <li class="answer-item" week="3">
-        <div>
-          <h3>week3</h3>
-          <button class="detail-button">詳細</button>
-        </div> 
-        <ul class="answer-content-list">
-          <li>
-            <p>Gitコマンドをインストールしましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>"GitHubのposse-apにあるリポジトリ「template-ph1-website」をforkできましたか？
-              ※リンク: https://github.com/posse-ap/template-ph1-website"</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>forkした「template-ph1-website」をcloneできましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-        </ul>
-      </li>
-
-      <li class="answer-item" week="4">
-        <div>
-          <h3>week4</h3>
-          <button class="detail-button">詳細</button>
-        </div> 
-        <ul class="answer-content-list">
-          <li>
-            <p>htmlファイルの中でstyle指定をしていないことを確認しましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>全体的な見た目はデザインと同じになっていますか？</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>"メインビジュアルを作成できましたか？
-              - 画像と見出しが被さるような見た目で実装できましたか？
-              - 「Scroll Down」を左下に配置できましたか？"</p>
-            <button>ヒントへ</button>
-          </li>
-        </ul>
-      </li>
-
-    </ul>
-
-  </section>
-</body>
-</html>
+<li class="answer-item" week="4">
+  <div>
+    <h3>week4</h3>
+    <button class="detail-button">詳細</button>
+  </div> 
+  <ul class="answer-content-list">
+    <li>
+      <p>htmlファイルの中でstyle指定をしていないことを確認しましたか？  </p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>全体的な見た目はデザインと同じになっていますか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>"メインビジュアルを作成できましたか？
+        - 画像と見出しが被さるような見た目で実装できましたか？
+        - 「Scroll Down」を左下に配置できましたか？"</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
+@endsection
--- src/resources/views/answerPh2.blade.php
+++ src/resources/views/answerPh2.blade.php
@@ -1,113 +1,87 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TechApp</title>
-  <script src="{{asset('/js/jquery-3.6.1.min.js')}}" defer></script>
-  <script src="{{asset('/js/accordion.js')}}" defer></script>
-</head>
-<body>
-  @include('components.header')
+@extends('layouts.each_ph_answer')
 
-  {{-- このファイルではph1向けに書いているが、本来は初めのボタン選択で中身を各ph仕様に変えていく --}}
-  <section>
-    <div>
-      <h2>week絞り込み</h2>
-      <label for="week">week</label>
-      <input type="text" name="week" id="week">
-      <input type="button" value="検索" id="filter-button">
-    </div>
+@section('answer-items')
+<li class="answer-item" week="17">
+  <div>
+    <h3>week17</h3>
+    <button class="detail-button">詳細</button>
+  </div>
+  <ul class="answer-content-list">
+    <li>
+      <p>Dockerをインストール出来ましたか？	</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>テンプレートをfork&clone出来ましたか？	</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>docker-compose up を実行しましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
 
-    <ul class="answer-list">
+<li class="answer-item" week="18">
+  <div>
+    <h3>week18</h3>
+    <button class="detail-button">詳細</button>
+  </div> 
+  <ul class="answer-content-list">
+    <li>
+      <p>https://it-biz.online/it-skills/database/ を読んで、内容を理解しましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>問題テーブル、選択肢テーブルの設計は理解できましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>正規化について理解できましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
 
-      <li class="answer-item" week="17">
-          <div>
-            <h3>week17</h3>
-            <button class="detail-button">詳細</button>
-          </div>
-          <ul class="answer-content-list">
-            <li>
-              <p>Dockerをインストール出来ましたか？	</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>テンプレートをfork&clone出来ましたか？	</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>docker-compose up を実行しましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-          </ul>
-      </li>
+<li class="answer-item" week="19">
+  <div>
+    <h3>week19</h3>
+    <button class="detail-button">詳細</button>
+  </div> 
+  <ul class="answer-content-list">
+    <li>
+      <p>phpMyAdminの画面を開くことが出来まし たか？	</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>questionsテーブルを作成することが出  来ましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>choicesテーブルを作成することが出来  ましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
 
-      <li class="answer-item" week="18">
-          <div>
-            <h3>week18</h3>
-            <button class="detail-button">詳細</button>
-          </div> 
-          <ul class="answer-content-list">
-            <li>
-              <p>https://it-biz.online/it-skills/database/ を読んで、内容を理解しましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>問題テーブル、選択肢テーブルの設計は理解できましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-            <li>
-              <p>正規化について理解できましたか？</p>
-              <button>ヒントへ</button>
-            </li>
-          </ul>
-      </li>
-
-      <li class="answer-item" week="19">
-        <div>
-          <h3>week19</h3>
-          <button class="detail-button">詳細</button>
-        </div> 
-        <ul class="answer-content-list">
-          <li>
-            <p>phpMyAdminの画面を開くことが出来ましたか？	</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>questionsテーブルを作成することが出来ましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>choicesテーブルを作成することが出来ましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-        </ul>
-      </li>
-
-      <li class="answer-item" week="20">
-        <div>
-          <h3>week20</h3>
-          <button class="detail-button">詳細</button>
-        </div> 
-        <ul class="answer-content-list">
-          <li>
-            <p>PDOについて理解できましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>dbconnect.phpを作成し、DB接続処理を実装出来ましたか？</p>
-            <button>ヒントへ</button>
-          </li>
-          <li>
-            <p>"正しい接続情報で、DB接続処理が正常に出来ることを確認しましたか？白い画面になります"</p>
-            <button>ヒントへ</button>
-          </li>
-        </ul>
-      </li>
-
-    </ul>
-
-  </section>
-</body>
-</html>
+<li class="answer-item" week="20">
+  <div>
+    <h3>week20</h3>
+    <button class="detail-button">詳細</button>
+  </div> 
+  <ul class="answer-content-list">
+    <li>
+      <p>PDOについて理解できましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>dbconnect.phpを作成し、DB接続処理を実装出来ましたか？</p>
+      <button>ヒントへ</button>
+    </li>
+    <li>
+      <p>"正しい接続情報で、DB接続処理が正常に出来ることを確認しました  か？白い画面になります"</p>
+      <button>ヒントへ</button>
+    </li>
+  </ul>
+</li>
+@endsection
