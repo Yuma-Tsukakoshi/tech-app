0289a1d4ad2bc9fc991c32744efef100b5cb3e97
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-03-11 10:46:34 +0900

    add:詳細ボタン押してPOSSE課題をslideDownで表示

---
+++ src/public/js/accordion.js
@@ -0,0 +1,22 @@
+$(() => {
+  // クリックイベントハンドラ
+  $(".answer-content-list").hide();
+  $(".detail-button").on("click", (e) => {
+    const $target = $(e.target);
+    // $targetから遡って一番近い.accordionを探す
+    const $accordion = $target.closest(".answer-item");
+    // $accordionから子要素の.bodyを探す
+    const $accordionBody = $accordion.children(".answer-content-list");
+
+    // ボディが可視状態か判定する
+    if ($accordionBody.is(":visible")) {
+      // 可視状態の場合
+      $accordion.removeClass("show");
+      $accordionBody.slideUp(150);
+    } else {
+      // 不可視状態の場合
+      $accordion.addClass("show");
+      $accordionBody.slideDown(150);
+    }
+  });
+});
