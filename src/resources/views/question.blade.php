127c191d8f183d8a905892526d10725d170caa5d
kazuki1023 <k.keio12569@gmail.com>
2023-03-11 23:03:46 +0000

    add: question.blade.phpにtailwindcssのパス追加

--- src/resources/views/question.blade.php
+++ src/resources/views/question.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TechApp</title>
+  @vite('resources/css/app.css')
   <script src="{{asset('/js/jquery-3.6.1.min.js')}}" defer></script>
 </head>
 <body>
