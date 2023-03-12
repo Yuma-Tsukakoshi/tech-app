f7a199e6c544a9b7b57dce711961d9f02317d1ce
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-03-12 17:32:45 +0900

    add:メインページ tailwind

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
-<header>
+<header class=" px-4 py-4 flex justify-between">
   <img src="{{asset('/img/logo.svg')}}" alt="">
-  <ul>
-    <li><a href="https://event.posse-ap.com/login">イベントアプリ</a></li>
-    <li><a href="">ログアウト</a></li>
+  <ul class="flex font-bold ">
+    <li><a href="https://event.posse-ap.com/login" class="p-4">イベントアプリ</a></li>
+    <li><a href="" class="p-4">ログアウト</a></li>
   </ul>
 </header>
--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TechApp</title>
+  @vite('resources/css/app.css')
   <script src="{{asset('/js/jquery-3.6.1.min.js')}}" defer></script>
   <script src="{{asset('/js/page_transition.js')}}" defer></script>
 </head>
@@ -12,23 +13,22 @@
   @include('components.header')
 
   <section>
-    <h1>ヒント一覧</h1>
-    <ul>
+    <ul class="mt-12">
       @for ($i = 1; $i <= 4; $i++)
-        <li>
-          <h2>ph{{$i}}</h2>
-          <p>ph{{$i}}の内容詳細</p>
-          <button id="transition-ph{{$i}}">ヒント一覧</button>
+        <li class="flex justify-evenly m-4 p-4 font-bold text-2xl">
+          <h2>Ph{{$i}}</h2>
+          <p>Ph{{$i}}の内容詳細</p>
+          <button id="transition-ph{{$i}}" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl ">ヒント一覧</button>
         </li>
       @endfor
     </ul>
   </section>
 
   {{-- 質問投稿フォーム --}}
-  <section>
-    <h1>質問投稿フォームはこちら</h1>
+  <section class="text-center font-bold mt-10">
+    <h1 class="text-xl">質問投稿フォームはこちら</h1>
     {{-- <button type="button" onclick="location.href='{{ route('./question') }}' ">質問</button> --}}
-    <button type="button" id="transition-question">質問</button>
+    <button type="button" id="transition-question" class="bg-blue-500 hover:bg-blue-400 text-white font-bold my-4 py-2 px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-2xl text-xl">質問</button>
   </section>
 </body>
 </html>
