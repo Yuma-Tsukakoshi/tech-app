419322eaeb8fee6d36ac8357669da17efc7607d9
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-03-16 15:33:34 +0900

    add: profile画面の tailwind

--- src/resources/views/components/profile.blade.php
+++ src/resources/views/components/profile.blade.php
@@ -1,24 +1,31 @@
-<div>
-  <figure>
-    <img src="" alt="">
-  </figure>
-  <div>
-    <tr>
-      <td>名前</td>
-      <td>ゆうま</td>
-    </tr>
-    <tr>
-      <td>出身</td>
-      <td>埼玉</td>
-    </tr>
-    <tr>
-      <td>趣味</td>
-      <td>バスケ、スノボ</td>
-    </tr>
-    <tr>
-      <td>一言</td>
-      <td>よろしく</td>
-    </tr>
+<div class="bg-slate-100 absolute z-10 justify-center top-1/2  left-1/2 -translate-x-1/2 -translate-y-2/4 p-10 rounded-3xl shadow-xl ">
+  <div class="flex">
+    <figure class="rounded-full h-40  w-40 relative bg-slate-300">
+      <img src="{{asset('/img/yuma.jpg')}}" alt="profile-img" class="rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/4">  
+    </figure>
+    <table>
+      <tbody class="font-bold text-lg text-center">
+        <tr class="border-b bg-neutral-100">
+          <td>名前</td>
+          <td>ゆうま</td>
+        </tr>
+        <tr class="border-b bg-white">
+          <td>出身</td>
+          <td>埼玉</td>
+        </tr>
+        <tr class="border-b bg-neutral-100">
+          <td>趣味</td>
+          <td>バスケ、スノボ</td>
+        </tr>
+        <tr class="border-b bg-white">
+          <td>一言</td>
+          <td>よろしく</td>
+        </tr>
+        <tr class="border-b bg-neutral-100">
+          <td>ステータスポイント：</td>
+          <td><span>0</span>pt</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <p>ステータスポイント：<span>0</span>pt</p>
 </div>
--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -11,7 +11,10 @@
     <script src="{{asset('js/page_transition.js')}}" defer></script>
 </head>
 
-<body>
+<body class="">
+    <div class="bg-black opacity-50 h-screen absolute top-0 left-0 w-screen"></div>
+    {{-- 本来はページを作るというよりは、モーダルで表示させたい！そこの遷移とか子要素として加えるのかとかは話し合って決めたい --}}
+
     @include('components.header')
     @include('components.profile')
 </body>
