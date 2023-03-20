1451f398f73094eff551dcb780e2b14d385b839c
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-03-20 12:58:10 +0900

    fix:tailwind ヒント一覧

--- src/resources/views/ph/answerPh1.blade.php
+++ src/resources/views/ph/answerPh1.blade.php
@@ -10,15 +10,15 @@
   <ul class="answer-content-list mx-8 bg-gray-50 shadow-xl">
     <li class="flex justify-between my-6 mx-2 p-2 font-bold bg-slate-100 rounded-xl shadow-xl text-xs md:text-sm lg:text-lg">
       <p class="text-[8px] md:text-sm">Google Chromeをインストールしましたか？</p>
-      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 md:py-1 md:px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl shadow-xl" id="transition-hint">ヒントへ</button>
+      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 md:py-1 md:px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl shadow-xl text-[8px] md:text-sm" id="transition-hint">ヒントへ</button>
     </li>
     <li class="flex justify-between my-6 mx-2 p-2 font-bold bg-slate-100 rounded-xl shadow-xl text-xs md:text-sm lg:text-lg">
       <p class="text-[8px] md:text-sm ">Visual Studio Codeをインストールしましたか？</p>
-      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 md:py-1 md:px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl shadow-xl text-xs md:text-sm lg:text-lg" id="transition-hint">ヒントへ</button>
+      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 md:py-1 md:px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl shadow-xl text-[8px] md:text-sm lg:text-lg" id="transition-hint">ヒントへ</button>
     </li>
     <li class="flex justify-between my-6 mx-2 p-2 font-bold bg-slate-100 rounded-xl shadow-xl text-xs md:text-sm lg:text-lg">
       <p class="text-[8px] md:text-sm">Visual Studio Codeの初期設定(editorの設定と日本語化)は出来ましたか？</p>
-      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 md:py-1 md:px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl shadow-xl text-xs md:text-xs lg:text-lg" id="transition-hint">ヒントへ</button>
+      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 md:py-1 md:px-6 border-b-4 border-blue-800 hover:border-blue-500 rounded-3xl shadow-xl text-[8px] md:text-sm" id="transition-hint">ヒントへ</button>
     </li>
   </ul>
 </li>
