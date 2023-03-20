72e5c4cce46fba4dce2b10dc83f061f6d0c05fc7
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-03-20 11:41:02 +0900

    add:tailwind header

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header class=" px-4 py-4 flex justify-between">
-  <img src="{{asset('/img/logo.svg')}}" alt="">
-  <ul class="flex font-bold ">
+  <img src="{{asset('/img/logo.svg')}}" alt="" class="w-16 md:w-32 lg:w-48">
+  <ul class="flex font-bold text-xs md:text-sm lg:text-lg">
     <li><a href="https://event.posse-ap.com/login" class="p-4">イベントアプリ</a></li>
     <li><a href="" class="p-4">ログアウト</a></li>
   </ul>
